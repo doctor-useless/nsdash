75de3c7f71e9fa23648a46ed1e0a661900119bb6
Joseph Innes <joeinnes0@gmail.com>
2019-11-27 21:05:03 +0100

    fix imports

--- functions/contacts-read.js
+++ functions/contacts-read.js
@@ -1,27 +1,26 @@
-/* Import faunaDB sdk */
-const faunadb = require('faunadb')
-import { getId } from './utils/getId'
+import { getId } from './utils/getId';
+const faunadb = require('faunadb');
 
-const q = faunadb.query
+const q = faunadb.query;
 const client = new faunadb.Client({
   secret: process.env.FAUNADB_SERVER_SECRET
-})
+});
 
 exports.handler = (event, context) => {
-  const id = getId(event.path)
-  console.log(`Function 'contact-read' invoked. Read id: ${id}`)
+  const id = getId(event.path);
+  console.log(`Function 'contact-read' invoked. Read id: ${id}`);
   return client.query(q.Get(q.Ref(`classes/contacts/${id}`)))
     .then((response) => {
-      console.log('success', response)
+      console.log('success', response);
       return {
         statusCode: 200,
         body: JSON.stringify(response)
-      }
+      };
     }).catch((error) => {
-      console.log('error', error)
+      console.log('error', error);
       return {
         statusCode: 400,
         body: JSON.stringify(error)
-      }
-    })
+      };
+    });
 }
--- functions/contacts-update.js
+++ functions/contacts-update.js
@@ -1,27 +1,27 @@
-const faunadb = require('faunadb')
-import { getId } from './utils/getId'
+import { getId } from './utils/getId';
+const faunadb = require('faunadb');
 
-const q = faunadb.query
+const q = faunadb.query;
 const client = new faunadb.Client({
   secret: process.env.FAUNADB_SERVER_SECRET
-})
+});
 
 exports.handler = (event, context) => {
-  const data = JSON.parse(event.body)
-  const id = getId(event.path)
-  console.log(`Function 'contact-update' invoked. update id: ${id}`)
-  return client.query(q.Update(q.Ref(`classes/contacts/${id}`), {data}))
+  const data = JSON.parse(event.body);
+  const id = getId(event.path);
+  console.log(`Function 'contact-update' invoked. update id: ${id}`);
+  return client.query(q.Update(q.Ref(`classes/contacts/${id}`), { data }))
     .then((response) => {
-      console.log('success', response)
+      console.log('success', response);
       return {
         statusCode: 200,
         body: JSON.stringify(response)
-      }
+      };
     }).catch((error) => {
-      console.log('error', error)
+      console.log('error', error);
       return {
         statusCode: 400,
         body: JSON.stringify(error)
-      }
-    })
+      };
+    });
 }
